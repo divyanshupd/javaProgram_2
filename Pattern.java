63f9cba4adcc9126def0e9585a89e13570b69524
divyanshupd <135193811+divyanshupd@users.noreply.github.com>
2024-08-20 11:26:58 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Pattern {
+	public static void StarTriangle(int n)
+	{
+		int a, b;
+		for (a = 0; a < n; a++) {
+			for (b = 0; b <= a; b++) {
+
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[])
+	{
+		int k = 5;
+		StarRightTriangle(k);
+	}
+}
